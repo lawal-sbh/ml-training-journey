097d824c170a66e753db037d2f6896c0ddb5bded
07sanad <07.sanad@gmail.com>
2026-03-30 08:45:07 +0100

    Week 1 Day 1 - SOECComparison class with best_algorithm and summary

---
+++ SOECComparison.py
@@ -0,0 +1,31 @@
+class   SOECComparison:
+    def __init__(self, results):
+        self.results = results
+    def best_algorithm(self):
+        if not self.results:
+            return None
+        best = self.results[0]
+        for results in self.results:
+            if  results["safety_rate"] > best["safety_rate"]:
+                best = results
+        return best["algorithm"]
+    def summary(self):
+        print("-" * 55)
+        print(f"{'Algorithm':<12} {'Safety Rate':>12} {'V_cell':>10} {'dT':>10}")
+        print("-" * 55)
+        for result in self.results:
+            print(f"{result['algorithm']:<12} {result['safety_rate']:>11}% {result['v_cell']:>10} {result['dT']:>10}")
+        print("-" * 55)
+
+results =[{"algorithm": "SAC", "safety_rate": 100.0, "v_cell": 1.28, "dT": -16.1},
+          {"algorithm": "TD3", "safety_rate": 100.0, "v_cell": 1.28, "dT": -15.8},
+]
+comp = SOECComparison(results)
+print("Best algorithm:", comp.best_algorithm())
+comp.summary()        
+
+
+       
+    
+
+    
